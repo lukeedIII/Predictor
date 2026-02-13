cc96ebbf77eead3d0e943d00d996084aec21f271
LukeedIII <luca.gnaegi@gmail.com>
2026-02-13 20:25:51 +0100

    chore: clean up repo  move test files and brainstorming to OLD-Stuff
    - Moved desktop test files (test_all, test_torch*) to OLD-Stuff/desktop_tests/
    - Moved brainstorming/ to OLD-Stuff/brainstorming/
    - Moved root tests/ to OLD-Stuff/old_tests/
    - Removed get-pip.py, temp_extract/, temp_wheels/ build leftovers

--- desktop/test_torch.py
+++
@@ -1,2 +0,0 @@
-import torch
-print('OK: ' + torch.__version__ + ', CUDA: ' + str(torch.cuda.is_available()))
--- desktop/test_torch2.py
+++
@@ -1,9 +0,0 @@
-import sys, traceback
-try:
-    import torch
-    sys.stdout.write('OK: ' + torch.__version__ + '\n')
-    sys.stdout.flush()
-except Exception as e:
-    sys.stdout.write('FAIL: ' + str(e)[:300] + '\n')
-    sys.stdout.flush()
-    traceback.print_exc()
